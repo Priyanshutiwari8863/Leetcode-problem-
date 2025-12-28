c4b5ba8d175ad11e2175ac2791f0bc350b2a39dc
Priyanshu Tiwari <162439047+Priyanshutiwari8863@users.noreply.github.com>
2025-12-29 01:43:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (90.55%) - LeetSync

---
+++ 290-word-pattern/word-pattern.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char, string> mp1;
+        unordered_map<string, char> mp2;
+        string word;
+        stringstream ss(s);
+        int i = 0;
+
+        while (ss >> word) {
+            if (i >= pattern.size()) return false;
+            char c = pattern[i];
+            if (mp1.count(c) && mp1[c] != word) return false;
+            if (mp2.count(word) && mp2[word] != c) return false;
+            mp1[c] = word;
+            mp2[word] = c;
+            i++;
+        }
+        return i == pattern.size();
+    }
+};
