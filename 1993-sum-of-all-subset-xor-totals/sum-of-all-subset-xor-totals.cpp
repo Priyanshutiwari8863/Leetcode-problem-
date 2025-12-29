dc45fa7430903d57c6fbb201a9cc9d6b36a72db0
Priyanshu Tiwari <162439047+Priyanshutiwari8863@users.noreply.github.com>
2025-12-29 20:40:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (36.35%) - LeetSync

---
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int orAll = 0;
+        for (int x : nums) {
+            orAll |= x;
+        }
+        return orAll * (1 << (nums.size() - 1));
+    }
+};
