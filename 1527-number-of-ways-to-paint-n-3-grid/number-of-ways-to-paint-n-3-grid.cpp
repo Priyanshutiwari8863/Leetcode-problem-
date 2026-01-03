7b757b78e7df5f04df493206495c7599d6c14a59
Priyanshu Tiwari <162439047+Priyanshutiwari8863@users.noreply.github.com>
2026-01-03 11:19:35 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.00%) - LeetSync

---
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numOfWays(int n) {
+        
+        long long mod = 1e9 + 7;
+
+        long long abc = 6;  // pattern ABC
+        long long aba = 6;  // pattern ABA
+
+        for (int i = 2; i <= n; i++) {
+            long long new_abc = (2 * abc + 2 * aba) % mod;
+            long long new_aba = (2 * abc + 3 * aba) % mod;
+
+            abc = new_abc;
+            aba = new_aba;
+        }
+
+        return (abc + aba) % mod;
+    }
+};
