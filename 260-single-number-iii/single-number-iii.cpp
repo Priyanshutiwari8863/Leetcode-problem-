1dda6357ca7bbda71d935367a6fbf4fffad3a5b6
Priyanshu Tiwari <162439047+Priyanshutiwari8863@users.noreply.github.com>
2026-05-13 13:08:25 +0530

    Time: 4 ms (16.47%) | Memory: 14.7 MB (17.57%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        vector<int> ans;
+       unordered_map<int, int> mp;
+
+        for(int i:nums){
+            mp[i]++;
+        }
+        for(auto it : mp){
+            if(it.second ==1){
+                ans.push_back(it.first);
+            }
+
+        }
+        return ans;
+    }
+};
