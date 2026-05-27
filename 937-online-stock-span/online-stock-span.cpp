4f638f30346a2cb0ce795c611e4f18d3465f4192
Priyanshu Tiwari <162439047+Priyanshutiwari8863@users.noreply.github.com>
2026-05-27 21:20:11 +0530

    Time: 19 ms (93.52%) | Memory: 91.9 MB (87.88%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.cpp
@@ -0,0 +1,25 @@
+#include <stack>
+#include <utility>
+
+class StockSpanner {
+private:
+    std::stack<std::pair<int, int>> st;
+
+public:
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int span = 1;
+        
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
+        
+        st.push({price, span});
+        
+        return span;
+    }
+};
