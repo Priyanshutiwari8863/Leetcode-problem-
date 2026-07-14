a14f2b2294340aa886931fcd8526bedbd89d92a0
Priyanshu Tiwari <162439047+Priyanshutiwari8863@users.noreply.github.com>
2026-07-14 22:02:22 +0530

    Time: 1177 ms (39.13%) | Memory: 386.4 MB (19.13%) - LeetSync

---
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int subsequencePairCount(vector<int>& nums) {
+        int MOD = 1e9 + 7;
+        int max_val = 200;
+        
+        vector<vector<int>> dp(max_val + 1, vector<int>(max_val + 1, 0));
+        dp[0][0] = 1;
+
+        for (int x : nums) {
+            vector<vector<int>> next_dp = dp;
+            
+            for (int g1 = 0; g1 <= max_val; ++g1) {
+                for (int g2 = 0; g2 <= max_val; ++g2) {
+                    if (dp[g1][g2] == 0) continue;
+                    
+                    int n_g1 = (g1 == 0) ? x : std::gcd(g1, x);
+                    next_dp[n_g1][g2] = (next_dp[n_g1][g2] + dp[g1][g2]) % MOD;
+                    
+                    int n_g2 = (g2 == 0) ? x : std::gcd(g2, x);
+                    next_dp[g1][n_g2] = (next_dp[g1][n_g2] + dp[g1][g2]) % MOD;
+                }
+            }
+            dp = move(next_dp);
+        }
+
+        long long total_pairs = 0;
+        for (int g = 1; g <= max_val; ++g) {
+            total_pairs = (total_pairs + dp[g][g]) % MOD;
+        }
+
+        return total_pairs;
+    }
+};
